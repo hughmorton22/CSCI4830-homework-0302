41e6da00990f57d0a421c6ef1689a1f5e167829c
hughmorton22 <79492913+hughmorton22@users.noreply.github.com>
2021-03-05 20:27:02 -0600

    Add files via upload

---
+++ workspaceCSCI4830-ex-0302-morton/simple-refactoring-examples/src/q1/extract_method/refactored/A.java
@@ -0,0 +1,45 @@
+package q1.extract_method.refactored;
+
+import java.util.List;
+
+public class A {
+   Node m1(List<Node> nodes, String p) {
+      // : Your answer
+	   ex(nodes, p);
+      // other implementation
+      return null;
+   }
+
+   Edge m2(List<Edge> edgeList, String p) {
+      // : Your answer
+	   ex(edgeList, p);
+      // other implementation
+      return null;
+   }
+
+   // : Your answer
+   <T extends Graph> void ex(List<T> objs, String p) {
+	   for(T obj : objs) {
+		   if (obj.contains(p)) {
+			   System.out.print(obj);;
+		   }
+	   }
+   }
+   // ? extractedMethod(?) {
+   //
+   // }
+}
+
+class Graph {
+	String name;
+
+	public boolean contains(String p) {
+	   return name.contains(p);
+	}
+}
+
+class Node extends Graph{
+}
+
+class Edge extends Graph{
+}
